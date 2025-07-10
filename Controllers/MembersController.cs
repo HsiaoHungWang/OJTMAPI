5d139d29ce62205786137d50638765e9786e8edf
HsiaoHungWang <wang@ispan.com.tw>
2025-07-10 20:18:11 +0800

    建立Members API，使用DTO設計回傳的欄位有哪些

---
+++ Controllers/MembersController.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using OJTMAPI.Models;
+using OJTMAPI.Models.Dto;
+
+namespace OJTMAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MembersController : ControllerBase
+    {
+        private readonly ClassDbContext _context;
+
+        public MembersController(ClassDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Members
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<MemberReadDto>>> GetMembers()
+        {
+             var members = await _context.Members.ToListAsync();
+
+            //把資料放進DTO
+            var memberDtos = members.Select(m => new MemberReadDto
+            {
+                MemberId = m.MemberId,
+                Name = m.Name,
+                Email = m.Email,
+                Age = m.Age,
+                FileName = m.FileName
+            });
+            
+
+            return Ok(memberDtos);
+        }
+
+        // GET: api/Members/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Member>> GetMember(int id)
+        {
+            var member = await _context.Members.FindAsync(id);
+
+            if (member == null)
+            {
+                return NotFound();
+            }
+
+            return member;
+        }
+
+        // PUT: api/Members/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutMember(int id, Member member)
+        {
+            if (id != member.MemberId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(member).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!MemberExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Members
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Member>> PostMember(Member member)
+        {
+            _context.Members.Add(member);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetMember", new { id = member.MemberId }, member);
+        }
+
+        // DELETE: api/Members/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteMember(int id)
+        {
+            var member = await _context.Members.FindAsync(id);
+            if (member == null)
+            {
+                return NotFound();
+            }
+
+            _context.Members.Remove(member);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool MemberExists(int id)
+        {
+            return _context.Members.Any(e => e.MemberId == id);
+        }
+    }
+}
---
+++ Models/Dto/MemberReadDto.cs
@@ -0,0 +1,15 @@
+﻿namespace OJTMAPI.Models.Dto
+{
+    public class MemberReadDto
+    {
+        public int MemberId { get; set; }
+
+        public string? Name { get; set; }
+
+        public string? Email { get; set; }
+
+        public int? Age { get; set; }
+
+        public string? FileName { get; set; }
+    }
+}
